982fcd6b8d1b0fa2f05b84db012003024f61b9b3
Gabriel Ruiz <gruiz17@berkeley.edu>
2014-11-24 05:18:32 -0800

    concurrency

--- main.go
+++ main.go
@@ -103,10 +103,11 @@ func (w weatherUnderground) temperature(city string) (float64, error) {
 	return kelvin, nil
 }
 
-// func temperature(city string, providers ...weatherProvider) (float64, error) {
+// not-concurrent version
+// func (w multiWeatherProvider) temperature(city string) (float64, error) {
 // 	sum := 0.0
 
-// 	for _, provider := range providers {
+// 	for _, provider := range w {
 // 		k, err := provider.temperature(city)
 // 		if err != nil {
 // 			return 0, err
@@ -115,19 +116,34 @@ func (w weatherUnderground) temperature(city string) (float64, error) {
 // 		sum += k
 // 	}
 
-// 	return sum / float64(len(providers)), nil
+// 	return sum / float64(len(w)), nil
 // }
 
+// concurrent version
 func (w multiWeatherProvider) temperature(city string) (float64, error) {
-	sum := 0.0
+	temps := make(chan float64, len(w))
+	errs := make(chan error, len(w))
 
 	for _, provider := range w {
-		k, err := provider.temperature(city)
-		if err != nil {
+		go func(p weatherProvider) {
+			k, err := p.temperature(city)
+			if err != nil {
+				errs <- err
+				return
+			}
+			temps <- k
+		}(provider)
+	}
+
+	sum := 0.0
+
+	for i := 0; i < len(w); i++ {
+		select {
+		case temp := <-temps:
+			sum += temp
+		case err := <-errs:
 			return 0, err
 		}
-
-		sum += k
 	}
 
 	return sum / float64(len(w)), nil
